b3b86e5a658a3965bdb928b417958ca212d03a0f
Tristan <tristanpinzari@example.com>
2026-02-08 16:58:09 -0500

    Set up Gigalixir deployment with Elixir Releases

--- config/runtime.exs
+++ config/runtime.exs
@@ -56,14 +56,11 @@ if config_env() == :prod do
   config :sample_app, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :sample_app, SampleAppWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    server: true,
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      ip: {0, 0, 0, 0},
+      port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
 
