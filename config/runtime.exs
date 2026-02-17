35dfbf983925e565197219bc4e0eda4b6c3ba363
Tristan <tristanpinzari@example.com>
2026-02-17 18:48:21 -0500

    trying to connect to bucket

--- config/runtime.exs
+++ config/runtime.exs
@@ -132,6 +132,11 @@ config :waffle,
   bucket: System.get_env("AWS_S3_BUCKET"),
   asset_host: System.get_env("ASSET_HOST")
 
+config :ex_aws, :s3,
+  scheme: "https://",
+  host: "s3.ca-central-1.amazonaws.com",
+  region: System.get_env("AWS_REGION")
+
 config :ex_aws,
   access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
   secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
